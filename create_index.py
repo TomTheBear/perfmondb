77f0f260ce9b4e9c36de852d47ed83e1e09adcc9
Thomas Roehl <thomas.roehl@fau.de>
2020-02-24 15:43:05 +0100

    Create index file python script

---
+++ create_index.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os, glob, os.path, re
+
+files = {"IVB" : "index/ivybridge.html",
+         "CLX" : "index/cascadelake_server.html",
+         "IVT" : "index/ivytown.html",
+         "ICL" : "index/icelake.html",
+         "GLM" : "index/goldmont.html",
+         "SLM" : "index/silvermont.html",
+         "GLP" : "index/goldmontplus.html",
+         "BDW" : "index/broadwell.html",
+         "HSW" : "index/haswell.html",
+         "BDW-DE" : "index/broadwell_server.html",
+         "BDX" : "index/broadwell_server.html",
+         "BNL" : "index/atom.html",
+         "HSX" : "index/haswell_server.html",
+         "JKT" : "index/snbep.html",
+         "KNL" : "index/knl.html",
+         "KNM" : "index/knl.html",
+         "NHM-EP" : "index/corei7.html",
+         "NHM-EX" : "index/corei7b.html",
+         "WSM-EP-DP" : "index/corei7wdp.html",
+         "WSM-EP-SP" : "index/corei7wsp.html",
+         "WSM-EX" : "index/wsmex.html",
+         "SKL" : "index/skylake.html",
+         "SKX" : "index/skylake_server.html",
+         "SNB" : "index/snb.html", 
+         "SNR" : None}
+
+
+outfile = "index.html"
+
+out = open(outfile, "w")
+
+
+out.write(""" <!DOCTYPE html>
+<html>
+<head>
+<title>Performance Monitoring event lists for Intel processors</title>
+</head>
+<body>\n""")
+
+for s in files:
+    p = files[s]
+    head = None
+    if p:
+        with open(p, "r") as fp:
+            data = fp.read()
+            m = re.search("<h3>(.*)</h3>", data)
+            if m:
+                head = m.group(1)
+    if p and head and s:
+        out.write("<h1>{}</h1>\n".format(head))
+        out.write("<p><a href={}>{}</a><br><a href={}>{}</a></p>\n\n".format(p, os.path.basename(p), s, s))
+
+#
+#<p>This is a paragraph.</p>
+
+out.write("""</body>\n</html>\n""")
